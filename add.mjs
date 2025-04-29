4837248f095beb3de918762a712d93b010272e6f
Rahul Kumar <143000302+rkumar2002@users.noreply.github.com>
2025-04-29 20:35:16 +0530

    First commit

---
+++ add.mjs
@@ -0,0 +1,7 @@
+function add(a, b) {
+    return a + b;
+}
+
+console.log(add(6, 3));
+
+export default add;
---
+++ diff.mjs
@@ -0,0 +1,7 @@
+function diff(a, b) {
+    return a - b;
+}
+
+console.log(diff(4, 2));
+
+export default diff;
